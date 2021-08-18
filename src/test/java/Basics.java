5989945fdb588054835be7a016da101dcd8190dc
fahadaziz85 <Jamadiulawwal1440_>
2021-08-18 17:34:51 +0200

    Added one END TO END Test case for Adding (Create POST), Updating (PUT), and Verifying Place with GET HTTP Method

--- src/test/java/Basics.java
+++ src/test/java/Basics.java
@@ -1,6 +1,10 @@
+import files.PayLoad;
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import org.testng.Assert;
+
 import static io.restassured.RestAssured.*;
-import java.awt.*;
+import static org.hamcrest.Matchers.*;
 
 public class Basics {
     public static void main(String[] args) {
@@ -15,33 +19,61 @@ public class Basics {
          */
 
         // Rest assured BaseURI
-
+        // First basic example
         RestAssured.baseURI = "https://rahulshettyacademy.com";
         // it will also log all the process of sending request and response
         given().log().all().queryParam("key", "qaclick123")
                 .header("content-Type", "application/json")
-                .body("{\n" +
-                        "  \"location\": {\n" +
-                        "    \"lat\": -38.383494,\n" +
-                        "    \"lng\": 33.427362\n" +
-                        "  },\n" +
-                        "  \"accuracy\": 50,\n" +
-                        "  \"name\": \"Frontline house\",\n" +
-                        "  \"phone_number\": \"(+92) 300 1234567\",\n" +
-                        "  \"address\": \"Some where on planet Earth\",\n" +
-                        "  \"types\": [\n" +
-                        "    \"shoe park\",\n" +
-                        "    \"shop\"\n" +
-                        "  ],\n" +
-                        "  \"website\": \"http://gofadi.com\",\n" +
-                        "  \"language\": \"French-IN\"\n" +
-                        "}")
+                .body(PayLoad.addPlace())
                 .when().post("/maps/api/place/add/json") // submitting the API request with POST HTTP method to the resource
-                .then().log().all().assertThat().statusCode(200); // then validating the response code 200 from method I don't know
+                .then().log().all().assertThat().statusCode(200)
+                .body("scope", equalTo("APP"))
+                .header("server", "Apache/2.4.18 (Ubuntu)");
+
+        // then validating the response code 200 from method I don't know
         // I don't know the anatomy of then object and how and from where assertion is asserting (validating the response)
 
+        // Add Place -> Update Place with New Address -> Get Place to validate if New address is present in response
 
+        String response = given().log().all().queryParam("key", "qaclick123")
+                .header("content-Type", "application/json")
+                .body(PayLoad.addPlace())
+                .when().post("/maps/api/place/add/json") // submitting the API request with POST HTTP method to the resource
+                .then().assertThat().statusCode(200)
+                .body("scope", equalTo("APP"))
+                .header("server", "Apache/2.4.18 (Ubuntu)").extract().response().asString();
 
+        System.out.println("Response: " + response);
+
+        JsonPath jsonPath = new JsonPath(response); // for parsing Json as String. So it takes Json as String argument.
+        String placeID = jsonPath.getString("place_id");
+        System.out.println("Here is the required Place Id: "  + placeID);
+
+        given().log().all().queryParam("key", "qaclick123")
+                .header("content-Type", "application/json")
+                .body("{\n" +
+                        "\n" +
+                        "\"place_id\" : \"" + placeID + "\" ,\n" +
+                        "\"address\" : \"17 Loxford lane, Ilford London\",\n" +
+                        "\"key\" : \"qaclick123\"\n" +
+                        "\n" +
+                        "}").when().put("/maps/api/place/update/json")
+                .then().assertThat().log().all().statusCode(200).body("msg", equalTo("Address successfully updated"));
+
+        // Get the Address
+
+        response = given().log().all().queryParam("key", "qaclick123")
+
+                .queryParam("place_id", placeID)
+                .when().get("/maps/api/place/get/json")
+                .then().assertThat().log().all().statusCode(200).extract().response().asString();
+
+        JsonPath jsonPath1 = new JsonPath(response); // for parsing Json as String. So it takes Json as String argument.
+        String address = jsonPath1.getString("address");
+        System.out.println("Here is the required Address: "  + address);
+
+        // TestNG assertion
+        Assert.assertEquals(address, "17 Loxford lane, Ilford London");
 
     }
 }
