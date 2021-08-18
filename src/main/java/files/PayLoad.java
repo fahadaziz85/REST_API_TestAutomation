4cb3118e642ec88f5dac889e093a70eb626f1e87
fahadaziz85 <Jamadiulawwal1440_>
2021-08-18 17:36:00 +0200

    Added one END TO END Test case for Adding (Create POST), Updating (PUT), and Verifying Place with GET HTTP Method

---
+++ src/main/java/files/PayLoad.java
@@ -0,0 +1,24 @@
+package files;
+
+public class PayLoad {
+
+    public static String addPlace(){
+
+        return "{\n" +
+                "  \"location\": {\n" +
+                "    \"lat\": -38.383494,\n" +
+                "    \"lng\": 33.427362\n" +
+                "  },\n" +
+                "  \"accuracy\": 50,\n" +
+                "  \"name\": \"Frontline house\",\n" +
+                "  \"phone_number\": \"(+92) 300 1234567\",\n" +
+                "  \"address\": \"Some where on planet Earth\",\n" +
+                "  \"types\": [\n" +
+                "    \"shoe park\",\n" +
+                "    \"shop\"\n" +
+                "  ],\n" +
+                "  \"website\": \"http://google.com\",\n" +
+                "  \"language\": \"French-IN\"\n" +
+                "}";
+    }
+}
