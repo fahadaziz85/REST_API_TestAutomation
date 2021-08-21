1430ce755fdd48d7079f067fb57a83185e60bbc8
fahadaziz85 <Jamadiulawwal1440_>
2021-08-21 13:21:32 +0200

    Added some meaningful comments for readability and understanding

--- src/test/java/Basics.java
+++ src/test/java/Basics.java
@@ -33,8 +33,11 @@ public class Basics {
         // then validating the response code 200 from method I don't know
         // I don't know the anatomy of then object and how and from where assertion is asserting (validating the response)
 
+        // End to End Usecase / Testcase.
+
         // Add Place -> Update Place with New Address -> Get Place to validate if New address is present in response
 
+        // STEP1: Add Place
         String response = given().log().all().queryParam("key", "qaclick123")
                 .header("content-Type", "application/json")
                 .body(PayLoad.addPlace())
@@ -46,9 +49,12 @@ public class Basics {
         System.out.println("Response: " + response);
 
         JsonPath jsonPath = new JsonPath(response); // for parsing Json as String. So it takes Json as String argument.
-        String placeID = jsonPath.getString("place_id");
-        System.out.println("Here is the required Place Id: "  + placeID);
 
+
+        String placeID = jsonPath.getString("place_id"); // Grab the place Id so that it can be used to update the place
+        System.out.println("Here is the required Place Id: " + placeID);
+
+        // STEP2: Update the place address
         given().log().all().queryParam("key", "qaclick123")
                 .header("content-Type", "application/json")
                 .body("{\n" +
@@ -60,7 +66,7 @@ public class Basics {
                         "}").when().put("/maps/api/place/update/json")
                 .then().assertThat().log().all().statusCode(200).body("msg", equalTo("Address successfully updated"));
 
-        // Get the Address
+        // STEP3: Get the Address
 
         response = given().log().all().queryParam("key", "qaclick123")
 
@@ -70,7 +76,7 @@ public class Basics {
 
         JsonPath jsonPath1 = new JsonPath(response); // for parsing Json as String. So it takes Json as String argument.
         String address = jsonPath1.getString("address");
-        System.out.println("Here is the required Address: "  + address);
+        System.out.println("Here is the required Address: " + address);
 
         // TestNG assertion
         Assert.assertEquals(address, "17 Loxford lane, Ilford London");
